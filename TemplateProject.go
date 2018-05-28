d853d7e9a20b67732e5309fc67e2b0c53a4d4c6b
Chanwit Piromplad <kingkong2103@gmail.com>
2018-05-28 17:47:20 +0700

    add dynamodbtemplate

--- TemplateProject.go
+++ TemplateProject.go
@@ -4,4 +4,5 @@ var templateProject = map[string]string{
 	"simple":      "https://github.com/golamb/golamb-simple-template.git",
 	"api-gateway": "https://github.com/golamb/golamb-api-gateway-template.git",
 	"gin":         "https://github.com/golamb/golamb-gin-template.git",
+	"dynamodb":    "https://github.com/golamb/golamb-dynamodb-template.git",
 }
